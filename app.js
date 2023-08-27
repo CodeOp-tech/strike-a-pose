7c84d7ef7c58d4b8eb07fc0f4ad870f811fd93ec
Gabriela Andrade <andradeojedagabriela@gamil.com>
2023-08-27 20:58:41 +0200

    .env

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+// 1. install dependecies
+// 2. import dependecies
+// 3. set up webcam and CanvasCaptureMediaStreamTrack
+// 4. define renferencies for those
+// 5. Load posetNet
+// 6. Detect function
+// 7. Drawing utilities from TensorFlow
+// 8. Draw function
 
-const cors = require('cors'); // add at the top
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require("cors"); // add at the top
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const { runInThisContext } = require("vm");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors()); // add after 'app' is created
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
