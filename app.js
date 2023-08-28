a576fda31d7b752bacaa85ceb635218d3dad3f4c
Kalliopi Ortitziou <kortitziou2@gmail.com>
2023-08-28 15:22:59 +0200

    Refactor Components

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors()); // add after 'app' is created
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
